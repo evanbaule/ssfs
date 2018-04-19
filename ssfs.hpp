21535e5ab0a3c6c98f67d64b262510900def62cf
Evan Baule <evanbaule@Evans-MacBook-Pro-2.local>
2018-04-19 12:42:12 -0400

    added enum/request struct

--- ssfs.hpp
+++ ssfs.hpp
@@ -12,5 +12,25 @@ typedef struct {
   unsigned int doubleIndirect;
 } inode;
 
+enum tags {
+	create_tag,
+	import_tag,
+	cat_tag,
+	delete_tag,
+	write_tag,
+	read_tag,
+	list_tag,
+	shutdown_tag,
+};
+
+typedef struct {
+	tags tag; //Maps to disk operation
+	string fname1; //<SSFS File Name>
+	string fname2; //<Unix File Name>
+	char c; //<char>
+	uint start_byte; //<start byte>
+	uint num_bytes; //<num bytes>
+} request;
+
 
 #endif
